063fdf1ea6de6adaaf458db22ebb7e1824a9222b
nusnick <nick@nustechnology.com>
2016-09-04 17:26:29 +0700

    Add small mode to image when upload

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
   belongs_to :author
-  has_attached_file :image, styles: {large: "700x2000>"}
+  has_attached_file :image, styles: {small: "200x2000>", large: "700x2000>"}
   validates_attachment :image, presence: true, content_type: {content_type: ["image/jpeg","image"]}
 end
