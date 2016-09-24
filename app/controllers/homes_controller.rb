a103026ed3328f9f24e6f2825f0491569da7cada
nusnick <nick@nustechnology.com>
2016-09-24 08:41:32 +0700

    Add prize

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,8 @@
 class HomesController < ApplicationController
   def index
     @photos = Photo.all.page(params[:page])
+    @first_prize = Photo.where(prize: "Giải nhất").last
+    @second_prizes = Photo.where(prize: "Giải nhì")
+    @third_prizes = Photo.where("prize = 'Giải ba' or prize = 'Giải Ba' ")
   end
 end
